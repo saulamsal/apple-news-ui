e491ed987e358f3bf30b3a99c000881d004d0221
saulamsal <saulamsal@gmail.com>
2025-01-18 02:20:01 -0500

    wip

---
+++ ios/ScoresLiveActivity/ScoresLiveActivityBundle.swift
@@ -0,0 +1,17 @@
+//
+//  ScoresLiveActivityBundle.swift
+//  ScoresLiveActivity
+//
+//  Created by Saul Sharma on 1/18/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct ScoresLiveActivityBundle: WidgetBundle {
+    var body: some Widget {
+        ScoresLiveActivity()
+        ScoresLiveActivityLiveActivity()
+    }
+}
---
+++ ios/ScoresLiveActivity/ScoresLiveActivityLiveActivity.swift
@@ -0,0 +1,80 @@
+//
+//  ScoresLiveActivityLiveActivity.swift
+//  ScoresLiveActivity
+//
+//  Created by Saul Sharma on 1/18/25.
+//
+
+import ActivityKit
+import WidgetKit
+import SwiftUI
+
+struct ScoresLiveActivityAttributes: ActivityAttributes {
+    public struct ContentState: Codable, Hashable {
+        // Dynamic stateful properties about your activity go here!
+        var emoji: String
+    }
+
+    // Fixed non-changing properties about your activity go here!
+    var name: String
+}
+
+struct ScoresLiveActivityLiveActivity: Widget {
+    var body: some WidgetConfiguration {
+        ActivityConfiguration(for: ScoresLiveActivityAttributes.self) { context in
+            // Lock screen/banner UI goes here
+            VStack {
+                Text("Hello \(context.state.emoji)")
+            }
+            .activityBackgroundTint(Color.cyan)
+            .activitySystemActionForegroundColor(Color.black)
+
+        } dynamicIsland: { context in
+            DynamicIsland {
+                // Expanded UI goes here.  Compose the expanded UI through
+                // various regions, like leading/trailing/center/bottom
+                DynamicIslandExpandedRegion(.leading) {
+                    Text("Leading")
+                }
+                DynamicIslandExpandedRegion(.trailing) {
+                    Text("Trailing")
+                }
+                DynamicIslandExpandedRegion(.bottom) {
+                    Text("Bottom \(context.state.emoji)")
+                    // more content
+                }
+            } compactLeading: {
+                Text("L")
+            } compactTrailing: {
+                Text("T \(context.state.emoji)")
+            } minimal: {
+                Text(context.state.emoji)
+            }
+            .widgetURL(URL(string: "http://www.apple.com"))
+            .keylineTint(Color.red)
+        }
+    }
+}
+
+extension ScoresLiveActivityAttributes {
+    fileprivate static var preview: ScoresLiveActivityAttributes {
+        ScoresLiveActivityAttributes(name: "World")
+    }
+}
+
+extension ScoresLiveActivityAttributes.ContentState {
+    fileprivate static var smiley: ScoresLiveActivityAttributes.ContentState {
+        ScoresLiveActivityAttributes.ContentState(emoji: "😀")
+     }
+     
+     fileprivate static var starEyes: ScoresLiveActivityAttributes.ContentState {
+         ScoresLiveActivityAttributes.ContentState(emoji: "🤩")
+     }
+}
+
+#Preview("Notification", as: .content, using: ScoresLiveActivityAttributes.preview) {
+   ScoresLiveActivityLiveActivity()
+} contentStates: {
+    ScoresLiveActivityAttributes.ContentState.smiley
+    ScoresLiveActivityAttributes.ContentState.starEyes
+}
