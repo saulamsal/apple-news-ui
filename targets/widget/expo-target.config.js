839a10661043458b88f2025d68c8db71e1d9d98e
saulamsal <saulamsal@gmail.com>
2025-01-18 02:37:40 -0500

    comeback later for activities

--- targets/widget/expo-target.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('@bacons/apple-targets/app.plugin').Config} */
-module.exports = {
-  type: "widget",
-  entitlements: {
-    "com.apple.security.application-groups": ["group.com.sportapp.apple-news-ui.ScoresWidget"]
-  }
-};
