0dd82eff4dca564a89703ff7ed4228fda232d4a8
saulamsal <saulamsal@gmail.com>
2025-01-27 04:27:47 -0500

    og titles

--- app/+html.tsx
+++ app/+html.tsx
@@ -14,6 +14,17 @@ export default function Root({ children }: PropsWithChildren) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
+        <meta property="og:title" content="Apple News UI - Find News & Topics" />
+        <meta property="og:description" content="Search through millions of articles, topics, and trusted sources to find the news that matters to you." />
+        <meta property="og:image" content="../assets/images/featured.png" />
+        <meta property="og:url" content="https://apple-news.expo.app/audio" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Apple News UI - Find News & Topics" />
+        <meta name="twitter:description" content="Search through millions of articles, topics, and trusted sources to find the news that matters to you." />
+        <meta name="twitter:image" content="../assets/images/featured.png" />
+
         {/*
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
           However, body scrolling is often nice to have for mobile web. If you want to enable it, remove this line.
